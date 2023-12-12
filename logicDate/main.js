1e600766692745a59c111662bf82567bb81c58ea
thuanWorkspace <117074155+thuanWorkspace@users.noreply.github.com>
2023-12-12 23:28:34 +0700

    'commit'

---
+++ logicDate/main.js
@@ -0,0 +1,30 @@
+var prop = new Date();
+var today = new Date();
+var currentMonth = 12;//current month
+var numberDay = 30;//month of current has which day.
+var userChoose = prop;//init user choose equals 
+var max_arr_size = 9;
+var arr = [];
+//init data
+//left side
+var leftDistance = prop - today;//psudo code
+for (let index = 0; index < leftDistance; index++) {
+    
+}
+//right side
+var rightDistance = max_arr_size - leftDistance;
+for (let index = 0; index < rightDistance; index++) {
+    
+}
+function btnNext(){
+
+}
+function btnPrev(){
+
+}
+function getNextDate(currentDate){
+
+}
+function getPrevDate(currentDate){
+
+}
